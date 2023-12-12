6f48c2634ce8efb112e96be58ecfdc7894ce240a
mattiabenelli <benellimattia0@gmail.com>
2023-12-12 11:40:36 +0100

    button add user with modal(no api call)

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
-export class AddEmployeeComponent {
+export class AddEmployeeComponent implements OnInit{
+
+  onSubmit(form:any){
+    console.log(form)
+  }
+
+  onSubmitReactive(){
+    console.log(this.aggiungiUtenteForm.value)
+  }
+  
+  aggiungiUtenteForm!: FormGroup;
+  
+  
+  ngOnInit(): void {
+    this.aggiungiUtenteForm = new FormGroup({
+      nome: new FormControl(null, Validators.required),
+      cognome: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      ruolo: new FormControl(null, [Validators.required]),
+    });
+  }
 
 }
