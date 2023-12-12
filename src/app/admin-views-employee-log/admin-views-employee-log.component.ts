4bb90963c5435743bb0826abe41eb23c4d81e136
alberto.bejenaru <Alberto.bejenaru@edu.iisleviponti.it>
2023-12-13 00:42:39 +0100

    merge risolto

--- src/app/admin-views-employee-log/admin-views-employee-log.component.ts
+++ src/app/admin-views-employee-log/admin-views-employee-log.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-views-employee-log',
   templateUrl: './admin-views-employee-log.component.html',
   styleUrls: ['./admin-views-employee-log.component.css']
 })
-export class AdminViewsEmployeeLogComponent {
+export class AdminViewsEmployeeLogComponent  implements OnInit  {
+  datiUtente: any;
+  idUtente: number | undefined;
 
+  ngOnInit(): void {
+
+    const userData = localStorage.getItem('utenteLoggato');
+
+    if (userData) {
+      this.datiUtente = JSON.parse(userData);
+      this.idUtente = this.datiUtente.id;
+    } else {
+      console.log('Nessun dato utente trovato nella Local Storage');
+    }
+  }
 }
